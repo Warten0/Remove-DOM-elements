aa6ffa208f7557ffeffd3ddf25ef7d273fbd6d9f
Warten0 Warten0 <1524763508@qq.com>
2023-05-29 02:23:28 +0000

    SAVE

---
+++ redo/web/main.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+
+final InputElement toDoInput = querySelector('#to-do-input') as InputElement;
+final UListElement toDoList = querySelector('#to-do-list') as UListElement;
+final ButtonElement deleteAll = querySelector('#delete-all') as ButtonElement;
+
+void main() {
+  toDoInput.onChange.listen(addToDoItem);
+  deleteAll.onClick.listen((_) => toDoList.children.clear());
+}
+
+void addToDoItem(Event e) {
+  final newToDo = LIElement()..text = toDoInput.value;
+  newToDo.onClick.listen((_) => newToDo.remove());
+  toDoInput.value = '';
+  toDoList.children.add(newToDo);
+}
